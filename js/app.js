ca3efb23584c8477dc3b79fc43198258a18ad103
amirhossein gharebaghi <mycaspian81@gmail.com>
2022-08-02 21:14:06 +0430

    finished project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn");
+const free = document.querySelectorlecror(".free");
+
+btn.addEventListener("click", function() {
+    free.style.display = "block";
+
+})
